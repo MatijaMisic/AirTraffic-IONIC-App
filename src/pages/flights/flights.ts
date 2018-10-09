479e1c70ec9d651ebe1a723fc4fcf0c105057186
MatijaMisic <mbackonja@ztech.io>
2018-10-09 15:40:45 +0200

    minor changes

--- src/pages/flights/flights.ts
+++ src/pages/flights/flights.ts
@@ -1,12 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Navbar } from 'ionic-angular';
 
-/**
- * Generated class for the FlightsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
